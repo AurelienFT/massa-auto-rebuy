a0dec30ba097248ad55a61a9e564066c647f2de9
AurelienFT <aurelien.foucault@epitech.eu>
2022-02-14 14:39:53 +0100

    Change the creation of the amount to buy a roll to have a more human-readable value.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod rpc;
 
-use std::{path::PathBuf};
+use std::{path::PathBuf, str::FromStr};
 
 use massa_wallet::Wallet;
 use anyhow::{Result};
@@ -20,7 +20,7 @@ async fn main(args: paw::Args) -> Result<()> {
     let wallet = Wallet::new(PathBuf::from("wallet.dat"))?;
     let wallet_info =  client.0.get_addresses(wallet.get_full_wallet().keys().copied().collect()).await;
     if let Ok(wallet_addresses) = wallet_info {
-        if !wallet_addresses.is_empty() && wallet_addresses[0].rolls.candidate_rolls == 0 &&  wallet_addresses[0].ledger_info.final_ledger_info.balance >= massa_models::Amount::from_raw(100000000000) {
+        if !wallet_addresses.is_empty() && wallet_addresses[0].rolls.candidate_rolls == 0 &&  wallet_addresses[0].ledger_info.final_ledger_info.balance >= massa_models::Amount::from_str("1").unwrap() {
             rpc::send_operation(&client, &wallet, massa_models::OperationType::RollBuy{ roll_count: 1 }, massa_models::Amount::from_raw(0), wallet_addresses[0].address, true).await?;
         }
     }
